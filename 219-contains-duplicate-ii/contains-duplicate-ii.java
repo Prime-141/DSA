4efb6508114229ad561fa7432cbac97042929128
Prime-141 <kumar.prinav08@gmail.com>
2026-07-18 01:07:21 +0530

    Time: 28 ms (14.97%) | Memory: 111.4 MB (5.10%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution {
+    
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        int n = nums.length;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0; i<n; i++)
+        {
+            if(map.containsKey(nums[i]))
+            {
+                int prev = map.get(nums[i]);
+
+                if( (i-prev)<= k)
+                {
+                    return true;
+                }
+            }
+            map.put(nums[i],i);
+        }
+        return false;
+
+       
+    }
+}
