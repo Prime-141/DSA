6b48cb07c16d4aace62851729e3e63095a92f4f1
Prime-141 <kumar.prinav08@gmail.com>
2026-07-23 22:17:59 +0530

    Time: 6 ms (90.21%) | Memory: 57.6 MB (83.23%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
@@ -1,7 +1,7 @@
 import java.util.*;
 class Solution {
     public List<Integer> findDuplicates(int[] nums) {
-        HashMap<Integer,Integer> map = new HashMap<>();
+     /*   HashMap<Integer,Integer> map = new HashMap<>();
 
         for(int num : nums)
         {
@@ -17,7 +17,26 @@ class Solution {
                 ans.add(key);
             }
         }
-        return ans;
+        return ans; */
+
+        int n = nums.length;
+        ArrayList<Integer> result = new ArrayList<>();
+
+        for(int i=0; i<n; i++)
+        {
+            int num = Math.abs(nums[i]);
+            int idx = num-1;
+
+            if(nums[idx] < 0)
+            {
+                result.add(num);
+            }
+            else
+            {
+                nums[idx] *= -1;
+            }
+        }
+        return result;
 
     }
 }
